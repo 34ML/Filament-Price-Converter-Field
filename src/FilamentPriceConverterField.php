43171e2d37438bf9df81589da47dfdf007632a1d
Mostafa Hassan <mostafa@34ml.com>
2023-06-01 16:59:26 +0300

    Add price field

--- src/FilamentPriceConverterField.php
+++ src/FilamentPriceConverterField.php
@@ -6,4 +6,15 @@ use Filament\Forms\Components\TextInput;
 
 class FilamentPriceConverterField extends TextInput
 {
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->numeric();
+
+        $this->afterStateHydrated(fn(TextInput $component, $state) => $component->state($state / 100));
+
+        $this->dehydrateStateUsing(fn ($state) => $state * 100);
+    }
 }
