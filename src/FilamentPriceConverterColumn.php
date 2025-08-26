ec482f47a44d48d03667bcae7b226c3fcc5b3862
Youssef Shaker <youssef@34ml.com>
2025-08-26 14:53:01 +0300

    fetch currency from tenant config

--- src/FilamentPriceConverterColumn.php
+++ src/FilamentPriceConverterColumn.php
@@ -3,13 +3,23 @@
 namespace _34ML\FilamentPriceConverterField;
 
 use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Facades\Config;
 
 class FilamentPriceConverterColumn extends TextColumn
 {
     public static function make(string $name): static
     {
         $field = parent::make($name);
-        $field->money(config('filament-price-converter-field.currency','USD'),100);
+
+        // Get currency from tenant config
+        $currencyCode   = Config::get('tenant.currency_code', 'USD');
+
+        // Format number without showing built-in currency symbol
+        $field->formatStateUsing(function ($state) {
+            return number_format($state / 100, 2); // divide by 100 if storing cents
+        })
+            ->suffix(" {$currencyCode}");
+
         return $field;
     }
 }
